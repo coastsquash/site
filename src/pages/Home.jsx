081601808895a937b1862ec82a095db4230cf91e
Geoff Johnson <geoff.jay@gmail.com>
2023-10-09 17:26:41 -0700

    feat: add restringing information

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { EmailLink, CallLink } from "../components";
+import { CallLink, EmailLink } from "../components";
 import { Panel } from "../components/Panel";
 
 const Home = () => (
@@ -31,12 +31,38 @@ const Home = () => (
       </div>
     </Panel.Section>
     <Panel.Section className="mt-4" title="Racket Restringing">
-      Sunshine Coast – Murray Smith – leave your racket with the staff at the the Gibsons rec centre and paper wrap your grip with your contact info.  Contact Murray to let him know that the racket is there.  He will contact you once the racket is restrung and returned to the Gibsons community center.
-      mujsmith34@gmail.com - (604) 885-7438
-      Evergreen (North Vancouver) - Cathy Covernton owns and manages the on-site CC Squash Pro Shop at Evergreen. She provides all of the sportswear, shoes and equipment you will need at competitive prices. Cathy also provides expert racquet stringing and will provide you with demo racquets for testing. Special orders? No problem.
-      Contact Cathy:
-      Email: ccsquashshop@gmail.com
-      Phone: 604-787-3097
+      <div className="flex flex-col gap-2">
+        <p>
+          Sunshine Coast – Murray Smith – leave your racket with the staff at the
+          Gibsons rec centre and paper wrap your grip with your contact info.
+          Contact Murray to let him know that the racket is there.  He will
+          contact you once the racket is restrung and returned to the Gibsons
+          community center.
+        </p>
+        <p>Contact Murray:</p>
+        <div>
+          Email: <EmailLink email="mujsmith34@gmail.com" inline>mujsmith34@gmail.com</EmailLink>
+        </div>
+        <div>
+          Phone: <CallLink phone="604-885-7438" inline>604-885-7438</CallLink>
+        </div>
+      </div>
+      <div className="flex flex-col gap-2">
+        <p>
+          Evergreen (North Vancouver) - Cathy Covernton owns and manages the
+          on-site CC Squash Pro Shop at Evergreen. She provides all of the
+          sportswear, shoes and equipment you will need at competitive prices.
+          Cathy also provides expert racquet stringing and will provide you with
+          demo racquets for testing. Special orders? No problem.
+        </p>
+        <p>Contact Cathy:</p>
+        <div>
+          Email: <EmailLink email="ccsquashshop@gmail.com" inline>ccsquashshop@gmail.com</EmailLink>
+        </div>
+        <div>
+          Phone: <CallLink phone="604-787-3097" inline>604-787-3097</CallLink>
+        </div>
+      </div>
     </Panel.Section>
   </Panel>
 );
